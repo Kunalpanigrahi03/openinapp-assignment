33051cd71268d7034d7892d679c793b71cdbcbfe
Kunal <Kunal@codewithkunal.com>
2024-08-29 16:11:53 +0530

    firebase credentials update

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAM_xvaGvoR8qEmVfMLtvMTuGC6BUo7uQ0",
-  authDomain: "openinapp-a91e1.firebaseapp.com",
-  projectId: "openinapp-a91e1",
-  storageBucket: "openinapp-a91e1.appspot.com",
-  messagingSenderId: "907135680595",
-  appId: "1:907135680595:web:ed20336fbe90923ead86a6",
-  measurementId: "G-E1BH51FRPD",
+  apiKey: "AIzaSyDcEUIavzlYXklvIqdYMOMaCcFXrSE18rU",
+  authDomain: "openinappassignment-acf63.firebaseapp.com",
+  projectId: "openinappassignment-acf63",
+  storageBucket: "openinappassignment-acf63.appspot.com",
+  messagingSenderId: "883556984849",
+  appId: "1:883556984849:web:ae4e73bb51ee78d71729b0"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
